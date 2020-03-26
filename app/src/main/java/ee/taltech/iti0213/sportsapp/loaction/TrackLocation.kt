e4715ee86f92783f2efccc776606dfe4c48a09de
taannu <tavo.annus@gmail.com>
2020-03-26 10:05:37 +0200

    Add track location domain object

---
+++ app/src/main/java/ee/taltech/iti0213/sportsapp/loaction/TrackLocation.kt
@@ -0,0 +1,32 @@
+package ee.taltech.iti0213.sportsapp.loaction
+
+import android.location.Location
+import android.os.Build
+import java.io.Serializable
+
+class TrackLocation(
+    val latitude: Double,
+    val longitude: Double,
+    val timestamp: Long,
+    val accuracy: Float,
+    val altitude: Double,
+    val altitudeAccuracy: Float
+) : Serializable {
+
+    companion object {
+        fun fromLocation(location: Location): TrackLocation {
+            val verticalAccuracy =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) location.verticalAccuracyMeters else 0f
+
+            return TrackLocation(
+                location.latitude,
+                location.longitude,
+                location.time,
+                location.accuracy,
+                location.altitude,
+                verticalAccuracy
+            )
+        }
+    }
+
+}
