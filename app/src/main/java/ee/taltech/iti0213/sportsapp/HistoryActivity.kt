5b6a56f00630a80745e3d6372076aab39ddd1fad
taannu <tavo.annus@gmail.com>
2020-04-17 16:19:03 +0300

    Better layout

--- app/src/main/java/ee/taltech/iti0213/sportsapp/HistoryActivity.kt
+++ app/src/main/java/ee/taltech/iti0213/sportsapp/HistoryActivity.kt
@@ -4,15 +4,33 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import ee.taltech.iti0213.sportsapp.db.DatabaseHelper
 import ee.taltech.iti0213.sportsapp.detector.FlingDetector
 
 class HistoryActivity : AppCompatActivity() {
 
+    private val databaseHelper: DatabaseHelper = DatabaseHelper(this)
+
     private lateinit var flingDetector: FlingDetector
+
+    private lateinit var scrollViewHistory: ScrollView
+    private lateinit var linearLayoutScrollContent: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        // scrollViewHistory = findViewById(R.id.scroll_history)
+        linearLayoutScrollContent = findViewById(R.id.linear_scroll)
+
+        databaseHelper.readTracksSummary(0, 999).forEach { track ->
+            run {
+                val trackView = layoutInflater.inflate(R.layout.track_history_item, linearLayoutScrollContent)
+                // linearLayoutScrollContent.addView(trackView)
+            }
+        }
 
         flingDetector = FlingDetector(this)
         flingDetector.onFlingRight = Runnable { onFlingRight() }
